6d2611fe880dd60462c0f9bd61ee7d84762b8149
nguyennhattruong0611 <truongnnde170427@fpt.edu.vn>
2025-07-03 01:04:51 +0700

    summit asm2 finish

--- StudentName_ClassCode_A01_BE/BusinessObjects/obj/Debug/net8.0/BusinessObjects.AssemblyInfo.cs
+++ StudentName_ClassCode_A01_BE/BusinessObjects/obj/Debug/net8.0/BusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb7d40d6358f7f39bb97d11a203d4183c5e187d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StudentName_ClassCode_A01_BE/DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
+++ StudentName_ClassCode_A01_BE/DataAccessObjects/obj/Debug/net8.0/DataAccessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb7d40d6358f7f39bb97d11a203d4183c5e187d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
